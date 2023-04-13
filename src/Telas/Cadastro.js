52ba6a997d24a635ef37f88ac01b80cd56e6de1c
Mateus Silva <83253336+Mateus046@users.noreply.github.com>
2023-04-13 20:20:30 -0300

    adição dos arquivos
    atualização dos arquivos

--- src/Telas/Cadastro.js
+++ src/Telas/Cadastro.js
@@ -1,60 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, ScrollView
  } from 'react-native';
+ import Estilo from './Estilo';
 
 const  Cadastro = ({navigation}) => {
   return (
   
-    <View style={styles.tela}>
+    <View style={Estilo.tela}>
         
-      <Image source={require('../../assets/Imagens/artmelogo4.png')} style={styles.image}/> 
+      <Image source={require('../../assets/Imagens/artmelogo4.png')} style={Estilo.image}/> 
 
-      <View style={styles.container}>
+      <View style={Estilo.containerCadastro}>
         
-          <View style ={ styles.titulo}>
+          <View style ={ Estilo.titulo}>
                 <TouchableOpacity onPress = {() => navigation.navigate('Login')}>
-                <Text style={styles.logo}>Login</Text>
+                <Text style={Estilo.logoCadastro}>Login</Text>
                 </TouchableOpacity>                
-                <Text style = {styles.textocadastrar}>Cadastrar</Text>
+                <Text style = {Estilo.textocadastrar}>Cadastrar</Text>
           </View>
 
-          <View style={styles.inputView}>
+          <View style={Estilo.inputView}>
             <TextInput
-          style={styles.inputText}
+          style={Estilo.inputText}
           placeholder="Nome"
           placeholderTextColor="#F97316"
             />
           </View>
 
-        <View style={styles.inputView}>
+        <View style={Estilo.inputView}>
         <TextInput
-          style={styles.inputText}
+          style={Estilo.inputText}
           placeholder="Email"
           placeholderTextColor="#F97316"
         />
         </View>
 
-        <View style={styles.inputView}>
+        <View style={Estilo.inputView}>
         <TextInput
-          style={styles.inputText}
+          style={Estilo.inputText}
           placeholder="Senha"
           placeholderTextColor="#F97316"
           secureTextEntry={true}
         />
         </View>
 
-        <View style= {styles.campos}>
-        <View style={styles.inputcurto}>
+        <View style= {Estilo.campos}>
+        <View style={Estilo.inputcurto}>
         <TextInput
-          style={styles.inputText}
+          style={Estilo.inputText}
           placeholder="Telefone"
           placeholderTextColor="#F97316"
         />
 
         </View>
-        <View style={styles.inputcurto}>
+        <View style={Estilo.inputcurto}>
         <TextInput
-          style={styles.inputText}
+          style={Estilo.inputText}
           placeholder="Estado"
           placeholderTextColor="#F97316"
              />
@@ -62,8 +63,8 @@ const  Cadastro = ({navigation}) => {
 
       </View>
 
-      <TouchableOpacity style={styles.loginButton} onPress={()=> navigation.navigate('Menu')}>      
-        <Text style={styles.loginText}>Entrar</Text>
+      <TouchableOpacity style={Estilo.loginButton} onPress={()=> navigation.navigate('Menu')}>      
+        <Text style={Estilo.loginText}>Entrar</Text>
       </TouchableOpacity>
 
       </View>
@@ -72,123 +73,5 @@ const  Cadastro = ({navigation}) => {
 
   );
 }
-
-const styles = StyleSheet.create({
-
-  container: {
-    borderRadius: 30,
-    paddingVertical: 20,
-    width: '80%',
-    backgroundColor: '#FFC700',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  //Texto do botão Login
-  logo: {  
-    textAlign: 'center',
-    borderRadius: 70,
-    borderWidth: 1,
-    borderColor: '#F97316',
-    alignContent: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: 'white',
-    padding: 14,
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#F97316',
-    marginBottom: 40,
-  },
-
-  //Texto do botão cadastrar
-    textocadastrar: {
-    textAlign: 'center',
-    alignContent: 'center',
-    justifyContent: 'center',
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: 'white',    
-    width: 130,
-    height: 50,
-    borderRadius: 70,
-    backgroundColor: '#F97316',
-    padding: 10,
-    },
-  
-  //input do Email e Senha
-  inputView: {
-    width: '80%',
-    backgroundColor: '#fff',
-    borderRadius: 25,
-    height: 45,
-    marginBottom: 20,
-    justifyContent: 'center',
-    padding: 20,
-  },
-
-  inputText: {
-    fontSize: 19,
-    height: 50,
-    color: 'orange',
-  },
-  
-  loginButton: {
-    textAlign: 'center',
-    width: '50%',
-    height: 15,
-    borderRadius: 40,
-    alignContent: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: '#F97316',
-    padding: 20,
-    fontSize: 10,
-    margin: 30,
-    },    
-
-  loginText: {
-    fontSize: 19,
-    color: 'white',
-    fontWeight: 'bold'
-  },
-
-  tela: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  image: {
-   height: 100,
-   width: 210,
-   margin: 30
-  },
-
-  campos: {
-    flexDirection: 'row',
-    flex:1,
-    justifyContent: 'center'   
-     
-   },
-
-   inputcurto: {
-    width: '20%',
-    backgroundColor: '#fff',
-    borderRadius: 25,
-    height: 45,
-    marginBottom: 10,
-    justifyContent: 'center',
-    marginHorizontal: 7,
-    padding: 20,
-    fontSize: 19,
-  },
-  titulo: {
-    flexDirection: 'row',
-    flex:1
-   }
-   
-});
 
 export default Cadastro;
