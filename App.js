230e02ec69fd3df25c63552bc3adf97a824e33a5
lucas1375 <lucasggg.striker@gmail.com>
2023-05-14 22:32:06 -0300

    categoria
    categoria

--- App.js
+++ App.js
@@ -7,20 +7,45 @@ import Menu from './src/Telas/Menu';
 import Esqueceu from './src/Telas/Esqueceu';
 import BoasVindas2 from './src/Telas/BoasVindas2';
 import Perfil from './src/Telas/Perfil';
+import Categoria from './src/Telas/Categoria';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const StackNavegacao =  () => {
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName='BoasVindas2'>
+       <Stack.Navigator initialRouteName='Categoria'>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="Menu" component={TabNavegacao} />
         <Stack.Screen name="Esqueceu" component={Esqueceu} />
-        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="Perfil" component={Perfil} options={{title:'Minha Conta', headerTitleAlign: 'center'}} />
         <Stack.Screen name="BoasVindas2" component={BoasVindas2} options={{headerShown:false}} />
+        <Stack.Screen name="Categoria" component={Categoria} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
+
 }
+
+const TabNavegacao =  () => {
+  return (
+    <NavigationContainer independent={true}>
+       <Tab.Navigator initialRouteName='Menu'>
+        <Tab.Screen name="Menu" component={Menu} options={{tabBarIcon: ({size, color }) => (
+          <MaterialCommunityIcons name='home' size= {12} color="#000000"/>
+        ) }} />
+        <Tab.Screen name="Perfil" component={Perfil} options={{title:'Minha Conta', headerTitleAlign: 'center'}} />
+        
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default StackNavegacao;
---
+++ src/Telas/Categoria.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, ScrollView } from 'react-native';
+
+const Categoria = () => {
+return (
+<View>
+    <Text style={styles.container}> Escreva algo</Text>
+</View>
+)
+};
+
+const styles = StyleSheet.create({
+    container: {
+        justifycontent: 'center',
+        textAlign: 'center',
+        
+    },
+});
+
+
+export default Categoria;
