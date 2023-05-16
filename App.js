e18d330c05f53e2d8dca313d25e350ec20e26078
lucas1375 <lucasggg.striker@gmail.com>
2023-05-16 15:39:56 -0300

    tela categoria
    categoria

--- App.js
+++ App.js
@@ -9,43 +9,43 @@ import BoasVindas2 from './src/Telas/BoasVindas2';
 import Perfil from './src/Telas/Perfil';
 import Categoria from './src/Telas/Categoria';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
-
 const Tab = createBottomTabNavigator();
 
-const StackNavegacao =  () => {
+const StackNavegacao = () => {
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName='Categoria'>
+      <Stack.Navigator initialRouteName='Menu'>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Menu" component={TabNavegacao} />
         <Stack.Screen name="Esqueceu" component={Esqueceu} />
-        <Stack.Screen name="Perfil" component={Perfil} options={{title:'Minha Conta', headerTitleAlign: 'center'}} />
-        <Stack.Screen name="BoasVindas2" component={BoasVindas2} options={{headerShown:false}} />
-        <Stack.Screen name="Categoria" component={Categoria} />
-       
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="BoasVindas2" component={BoasVindas2} options={{ headerShown: false }} />
+        <Stack.Screen name="Categoria" component={TabNavegacao} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
 }
 
-const TabNavegacao =  () => {
+const TabNavegacao = () => {
   return (
-    <NavigationContainer independent={true}>
-       <Tab.Navigator initialRouteName='Menu'>
-        <Tab.Screen name="Menu" component={Menu} options={{tabBarIcon: ({size, color }) => (
-          <MaterialCommunityIcons name='home' size= {12} color="#000000"/>
-        ) }} />
-        <Tab.Screen name="Perfil" component={Perfil} options={{title:'Minha Conta', headerTitleAlign: 'center'}} />
-        
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator initialRouteName='Menu'>
+      <Tab.Screen
+        name="Menu"
+        component={Menu}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name='home' size={12} color="#000000" />
+          ),
+        }}
+      />
+      <Tab.Screen name="Perfil" component={Perfil} />
+      <Tab.Screen name="Categoria" component={Categoria} />
+    </Tab.Navigator>
   );
 }
-
 
 export default StackNavegacao;
--- src/Telas/Categoria.js
+++ src/Telas/Categoria.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TextInput, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Categoria = () => {
-return (
-<View>
-    <Text style={styles.container}> Escreva algo</Text>
-</View>
-)
-};
+  return (
+    <View style={styles.beck}>
+      <Text style={styles.container}>
+        Escreva algo
+      </Text>
+
+      <View style={styles.categoriaContainer}>
+        <Text style={styles.pesquisa}>
+          Selecione uma categoria......
+        </Text>
+        <Image source={require('../../assets/Imagens/seta.png')} style={styles.seta} />
+      </View>
+
+      <Text style={styles.escreva}>
+        Escreva...
+      </Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        justifycontent: 'center',
-        textAlign: 'center',
-        
-    },
+  container: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    flexDirection: 'row'
+  },
+  pesquisa: {
+    margin: 12,
+    textDecorationLine: 'underline',
+    fontSize: 20
+  },
+  escreva: {
+    margin: 12
+  },
+  beck: {
+    backgroundColor: "white"
+  },
+  categoriaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 12
+  },
+  seta: {
+    
+    width: 30,
+    height: 30,
+    marginLeft: 5,
+    
+  }
 });
-
 
 export default Categoria;
